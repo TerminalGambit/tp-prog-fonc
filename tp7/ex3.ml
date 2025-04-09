c5a8114940859ed41f116bb96db13eaaf7fbda7c
TerminalGambit <jackmassey.monaco@outlook.com>
2025-04-09 17:11:20 +0200

    fact

--- tp7/ex3.ml
+++ tp7/ex3.ml
@@ -3,7 +3,7 @@ type 'a item = Item of 'a * 'a flot and 'a flot = 'a item Lazy.t ;;
 let scons x f = lazy (Item(x, f)) ;;
 let get_item = function lazy (Item(hd, tl)) -> (hd, tl) ;;
 
-let rec ints_from_strict n = scons n (ints_from_strict (n+1)) ;;
+(* let rec ints_from_strict n = scons n (ints_from_strict (n+1)) ;; *)
 let rec ints_from n = lazy(Item(n, (ints_from (n+1)))) ;;
 
 let shead s = 
@@ -21,14 +21,14 @@ let rec snth s n =
 
 let rec sfilter p s =
   let (hd, tl) = get_item s in
-  if p hd then scons hd (sfilter p tl)
+  if p hd then lazy(Item(hd, sfilter p tl))
   else sfilter p tl 
 ;;
 
 let rec smap2 f s1 s2 =
   let (hd1, tl1) = get_item s1 in
   let (hd2, tl2) = get_item s2 in
-  scons (f hd1 hd2) (smap2 f tl1 tl2)
+  lazy(Item((f hd1 hd2), (smap2 f tl1 tl2)))
 ;;
 
 let ( ++ ) = smap2 ( + ) ;;
@@ -38,7 +38,8 @@ let rec siter n f s =
   if n > 0 then
     let (hd, tl) = get_item s in
     f hd;
-    siter (n-1) f tl ;;
+    siter (n-1) f tl
+;;
 
 let sprint n s =
   print_string "[";
@@ -52,7 +53,7 @@ let sprint n s =
   in
   aux n s;
   print_string "]";
-  print_newline();
+  print_newline()
 ;;
 
 let () = sprint 10 (ints_from 0) ;;
@@ -60,7 +61,15 @@ let () = sprint 10 (ints_from 0) ;;
 (* let rec odds_from n = scons n (odds_from (n + 2)) ;; *)
 
 let is_odd = fun x -> x mod 2 = 1 ;;
-let odds_stream = sfilter is_odd (ints_from 0) 
+let odds_stream = sfilter is_odd (ints_from 0) ;;
 
 let () = sprint 10 odds_stream ;;
 
+let rec factorials =
+  let rec fact_aux n acc =
+    lazy(Item(acc, (fact_aux (n + 1) (acc * (n + 1)))))
+  in
+  fact_aux 0 1
+;;
+
+let () = sprint 10 factorials ;;
